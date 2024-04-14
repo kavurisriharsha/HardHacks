a67e2956f9fd9b49aac331a6ef6974004b98937f
kavurisriharsha <150100995+kavurisriharsha@users.noreply.github.com>
2024-04-13 19:35:42 -0700

    Create BL.ino

---
+++ BL.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3); // RX, TX pins for SoftwareSerial
+
+void setup() {
+  Serial.begin(9600); // Serial monitor for debugging
+  BTSerial.begin(9600); // Bluetooth module baud rate
+  
+  Serial.println("HC-05 Bluetooth Module Initialized");
+}
+
+void loop() {
+  // Check if data is available on Bluetooth module
+  String received;
+  if (BTSerial.available()) {
+    received = BTSerial.readString(); // Read the incoming byte from Bluetooth
+    
+    // Print the received character to Serial monitor
+    Serial.print("Received: ");
+    Serial.println(received);
+
+    int hrs, mins;
+
+    hrs = received.substring(0,received.indexOf(':')).toInt();
+    mins = received.substring(received.indexOf(':')+1, received.length()).toInt();
+
+    Serial.println(hrs);
+    Serial.println(mins);
+    
+  }
+}
